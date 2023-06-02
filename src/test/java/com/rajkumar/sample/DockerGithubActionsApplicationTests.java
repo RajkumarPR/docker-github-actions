31ba2cad3e18734a30ecafe21da4aea3828c6bf6
rajkumarpr <raju.raigonde@gmail.com>
2023-06-03 00:56:52 +0530

    added java code sample

---
+++ src/test/java/com/rajkumar/sample/DockerGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajkumar.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
